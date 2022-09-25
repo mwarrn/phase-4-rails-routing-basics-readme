477f487e96c6276521d93cd2b775abc8df98472d
Michael Warren <mwarrn@amazon.com>
2022-09-25 14:36:52 -0700

    Completed assignment

---
+++ app/controllers/cheeses_controller.rb
@@ -0,0 +1,8 @@
+class CheesesController < ApplicationController
+    def index
+        # model
+        cheeses = Cheese.order(price: :desc)
+        # view
+        render json: cheeses
+      end
+end
